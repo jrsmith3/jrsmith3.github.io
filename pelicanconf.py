2d42521e224223581fac2c489e110a5f15864873
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2013-08-31 13:17:18 -0400

    Updated config.

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,21 +2,22 @@
 # -*- coding: utf-8 -*- #
 
 AUTHOR = u'Joshua Ryan Smith'
-SITENAME = u'The Essential Beatfinger'
+# SITENAME = u'The Essential Beatfinger'
+SITENAME = u'Joshua Ryan Smith'
 SITEURL = ''
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = u'en'
 
 # Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+# LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+#           ('Python.org', 'http://python.org'),
+#           ('Jinja2', 'http://jinja.pocoo.org'),
+#           ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'http://twitter.com/joshua_r_smith'),
+          ('github', 'http://github.com/jrsmith3'),)
 
 DEFAULT_PAGINATION = False
